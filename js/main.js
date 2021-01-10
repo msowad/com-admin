3af9f358959026218f7e7a0687c738fc13e1eda7
Sowad Mim <mdsowad2004@gmail.com>
2021-01-10 14:24:52 +0600

    Building responsive header and sidebar.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 (function ($) {
+    "use strict";
     // Scroll Bar
     let jscr1 = $('.js-scrollbar1');
     if(jscr1[0]) {
@@ -12,5 +13,29 @@
         const that = $(this);
         const dotColor = that.data('color');
         that.css('background', dotColor);
+    });
+
+    // Side Bar Toggle
+    $(".btn-sidebar-toogle").on('click', function() {
+        $(".navbar-nav").toggleClass('navbar-show');
+        $(".body-overlay").toggleClass('overlay-show');
+    });
+
+    // Dropdown
+    const dropdown = $(".js-dropdown-button");
+    dropdown.on('click', function() {
+        const targetDropdown = $(this).data('target');
+        $("." + targetDropdown).toggleClass('js-dropdown-show not-focus-close');
+    });
+
+    // Close dropdown on click body
+    $(window).on('mouseup', function(event) {
+        const dropdowns = $(".js-dropdown-button");
+        dropdowns.each(function() {
+            const targetDropdown = $(this).data('target');
+            const element = $("." + targetDropdown);
+            if(event.target != element && event.target.parentNode != element)
+                $("." + targetDropdown).removeClass('js-dropdown-show not-focus-close');
+        });
     });
 })(jQuery)
